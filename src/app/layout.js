fee85ec12a7e6ece52d96f180583b4de1413f40e
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-08 20:05:12 +0500

    added main header with nav bar and simple styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        
+      <main className="max-w-4xl mx-auto p-4">
         {children}
+        </main>
         </body>
     </html>
   );
