426dc51315630bbfaa6866dbd98b62e37dd90a57
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-09 00:31:55 +0500

    integrated hero section

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -1,4 +1,4 @@
-import Right from "@/components/icons/Right";
+import Right from "../icons/Right";
 import Image from "next/image";
 
 export default function Hero() {
@@ -6,15 +6,14 @@ export default function Hero() {
     <section className="hero md:mt-4">
       <div className="py-8 md:py-12">
         <h1 className="text-4xl font-semibold">
-          Everything<br />
-          is better<br />
-          with a&nbsp;
+          Start your day<br />
+          with&nbsp;
           <span className="text-primary">
-            Pizza
+            A Fresh Tart
           </span>
         </h1>
         <p className="my-6 text-gray-500 text-sm">
-          Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life
+          Bring a little sweetness in your life with our selection of desserts
         </p>
         <div className="flex gap-4 text-sm">
           <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
@@ -28,7 +27,7 @@ export default function Hero() {
         </div>
       </div>
       <div className="relative hidden md:block">
-        <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={'pizza'} />
+        <Image src={'/cake.png'} layout={'fill'} objectFit={'contain'} alt={'Cake'} />
       </div>
     </section>
   );
