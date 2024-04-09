3dd823fc4631a1f15e006b5af6d0af901a2a8898
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-09 22:45:56 +0500

    added about us to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
-import HomeMenu from "./components/layout/HomeMenu"
+import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/SectionHeaders";
 
 export default function Home() {
   return (
@@ -8,6 +9,22 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders
+          subHeader ={'Our Story'}
+          mainHeader={'About Us'}
+          />
+
+      <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minima odit recusandae. Illum ipsa non repudiandae? Eum ipsam iste quos suscipit tempora? Aperiam esse fugiat inventore laboriosam officiis quam rem!
+          </p>
+          <p>At consectetur delectus ducimus est facere iure molestias obcaecati quaerat vitae voluptate? Aspernatur dolor explicabo iste minus molestiae pariatur provident quibusdam saepe?</p>
+          <p>Laborum molestias neque nulla obcaecati odio quia quod reprehenderit sit vitae voluptates? Eos, tenetur.</p>
+        </div>
+
+
+      </section>
     </>
   )
 }
