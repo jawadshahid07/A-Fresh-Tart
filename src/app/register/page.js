8d293d1ef060cbcedb0e43a2397e695d1d6df5c9
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-11 12:15:56 +0500

    added more form styles

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -46,10 +46,10 @@ export default function RegisterPage() {
         </div>
       )}
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-        <input type="email" placeholder="email" value={email}
+        <input type="email" placeholder="Email" value={email}
                disabled={creatingUser}
                onChange={ev => setEmail(ev.target.value)} />
-        <input type="password" placeholder="password" value={password}
+        <input type="password" placeholder="Password" value={password}
                disabled={creatingUser}
                 onChange={ev => setPassword(ev.target.value)}/>
         <button type="submit" disabled={creatingUser}>
@@ -62,7 +62,7 @@ export default function RegisterPage() {
           onClick={() => signIn('google', {callbackUrl:'/'})}
           className="flex gap-4 justify-center">
           <Image src={'/google.png'} alt={''} width={24} height={24} />
-          Login with google
+          Login with Google
         </button>
         <div className="text-center my-4 text-gray-500 border-t pt-4">
           Existing account?{' '}
