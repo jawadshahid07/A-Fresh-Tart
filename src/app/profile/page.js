e17d37bdb25963a9ca4bb6dca0c44d5d7c901c07
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-12 14:49:09 +0500

    added image with edit button, and name and email with save button

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -23,6 +23,21 @@ export default function ProfilePage() {
             <h1 className="text-center text-primary text-4xl mb-4">
                 Profile
             </h1>
+            <div className="max-w-md mx-auto">
+                <div className="flex gap-4 items-center">
+                    <div>
+                        <div className="p-2 rounded-lg relative">
+                            <Image className="rounded-lg w-full h-full mb-1" src={userImage} width={250} height ={250} alt={'avatar'} />
+                            <button type="button">Edit</button>
+                        </div>
+                    </div>
+                    <div className="grow">
+                        <input type="text" placeholder="first and last name" value={userName} onChange={ev => setUserName(ev.target.value)}/>
+                        <input type="email" disabled={true} value={session.data.user.email}/>
+                        <button type="submit">Save</button>
+                    </div>
+                </div>
+            </div>
         </section>
-    )
+    );
 }
