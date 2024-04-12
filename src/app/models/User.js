d3fd0c44234d053ba12d66756cd0c45af1372948
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-12 15:08:18 +0500

    moved encryption to register

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,24 +1,10 @@
-import bcrypt from 'bcrypt';
 import {model, models, Schema} from "mongoose";
 
 const UserSchema = new Schema({
+  name: {type: String},
   email: {type: String, required: true, unique: true},
-  password: {
-    type: String, 
-    required: true, 
-    validate: pass => {
-        if (!pass?.length || pass.length < 8) {
-          new Error('password must be atleast 5 characters');
-          return false;
-        }
-    },
-  },
+  password: {type: String},
+  image: {type: String},
 }, {timestamps: true});
-
-UserSchema.post('validate', function (user) {
-  const notHashedPassword = user.password;
-  const salt = bcrypt.genSaltSync(10);
-  user.password = bcrypt.hashSync(notHashedPassword, salt);
-})
 
 export const User = models?.User || model('User', UserSchema);
