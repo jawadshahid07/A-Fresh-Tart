58c5548d312248c97c2098332eff02f844ec2ce2
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-12 14:45:13 +0500

    initialised profile page with redirect on unauthenticated

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,5 +1,28 @@
+'use client'
+import {useSession} from "next-auth/react";
+import Image from "next/image";
+import {redirect} from "next/navigation";
+import {useState} from "react";
+
 export default function ProfilePage() {
-    <section>
-        <h1>Profile</h1>
-    </section>
+    const session = useSession();
+    const [userName, setUserName] = useState(session?.data?.user?.name || '');
+    const {status} = session;
+
+    if (status === 'loading') {
+        return 'Loading...';
+    }
+
+    if (status === 'unauthenticated') {
+        return redirect('/login');
+    }
+
+    const userImage = session.data.user.image;
+    return (
+        <section className="mt-8">
+            <h1 className="text-center text-primary text-4xl mb-4">
+                Profile
+            </h1>
+        </section>
+    )
 }
