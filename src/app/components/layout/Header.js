b404749c6f441ec95bab3841b9b2f2ac46ce513c
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-12 14:25:07 +0500

    edited header to include profile button and greeting message for user

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -4,8 +4,12 @@ import Link from "next/link";
 
 export default function Header() {
     const session = useSession();
-    console.log(session);
-    const status = session.status;
+    const status = session?.status;
+    const userData = session.data?.user;
+    let userName = userData?.name || userData?.email;
+    if (userName && userName.includes(' ')) {
+        userName = userName.split(' ')[0];
+    }
     return (
         <header className="flex items-center justify-between">
         
@@ -20,10 +24,16 @@ export default function Header() {
             </nav>
             <nav className="flex items-center gap-4 text-gray-500 font-semibold">
                 {status === 'authenticated' && (
-                    <button
-                     onClick={() => signOut()} className ="bg-primary rounded-full text-white px-8 py-2">
+                    <>
+                        <Link href={'/profile'} className="whitespace-nowrap">
+                        Hello, {userName}
+                        </Link>
+                        <button
+                        onClick={() => signOut()}
+                        className="bg-primary rounded-full text-white px-8 py-2">
                         Logout
-                    </button>
+                        </button>
+                  </>
                 )}
                 {status !== 'authenticated' && (
                     <>
