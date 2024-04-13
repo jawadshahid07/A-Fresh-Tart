505561eaba51f1ecfde99e371e816e836bf6c990
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-13 17:44:28 +0500

    initialized react project

--- src/app/api/upload/route.js
+++
@@ -1,7 +0,0 @@
-export async function POST(req) {
-    const data = await req.formData();
-    if (data.get('file')) {
-
-    }
-    return Response.json(true);
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#776B5D', secondary: '#F3EEEA', tertiary: '#B0A695', extra: '#EBE3D5'
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
