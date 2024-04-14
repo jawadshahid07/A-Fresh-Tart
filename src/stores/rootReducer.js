d0378fdebc471cbf83d8dbee15f4962cd5b472f3
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-14 18:54:15 +0500

    updated rootreducer to include cartReducer

--- src/stores/rootReducer.js
+++ src/stores/rootReducer.js
@@ -1,3 +1,10 @@
-const rootReducer = {};
+import { combineReducers } from "redux";
+import cartReducer from "./cart/cartSlice";
+
+const rootReducer = combineReducers(
+    {
+        cart: cartReducer
+    }
+);
 
 export default rootReducer;
