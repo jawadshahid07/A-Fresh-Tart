70e85957690271b300cd746e359ff709a48affdc
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-14 12:44:06 +0500

    added + button to carousel

--- src/components/ProductsPreview.jsx
+++ src/components/ProductsPreview.jsx
@@ -33,6 +33,10 @@ export const ProductsPreview = () => {
             .catch(e => console.log(e))
     }, [])
 
+    const onAddProduct = (product) => {
+        console.log(product);
+    }
+
     return (
         <div className="container mx-auto pb-4 w-2/3 text-white bg-black">
            <Carousel responsive={responsive}>
@@ -40,7 +44,7 @@ export const ProductsPreview = () => {
                 products.length > 0 && products.map((product, index) => {
                     return (
                         <div className="w-full p-3">
-                            <ProductPreviewCard key={index} product={product} />
+                            <ProductPreviewCard key={index} product={product} onAddProduct={onAddProduct} />
                         </div>
                     )
                 })
