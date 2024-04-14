7a8ef6ecd1ce27374837948144bc27d118d29386
jawadshahid07 <jawad.shahid70@outlook.com>
2024-04-14 19:06:52 +0500

    added product reducer to root reducer

--- src/stores/rootReducer.js
+++ src/stores/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cartSlice";
+import productReducer from "./menu/productsSlice";
 
 const rootReducer = combineReducers(
     {
-        cart: cartReducer
+        cart: cartReducer,
+        products: productReducer
     }
 );
 
